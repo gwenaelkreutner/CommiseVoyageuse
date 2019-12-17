de313efeb071955983c235447f4dd8b809bddab8
Lyza_Messadene <messadenelyza@gmail.com>
2019-12-17 02:19:16 +0100

    Ajout road

--- src/road.c
+++ src/road.c
@@ -1,30 +1,70 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "road.h"
 #include "town.h"
 
 struct road * createRoad (struct town * U, struct town * V) {
-    return 0;
+    struct road * R = (struct road *)calloc(1,sizeof(struct road));
+    R->U = U;
+    R->V = V;
+
+    return R;
 }
 
 void freeRoad ( struct road * R ) {
-  /* TOODO*/
+    free(R);
 }
 
 struct town * getURoad(struct road * R) {
-    return 0;
+    return R->U;
 }
 
 void setURoad ( struct road * R, struct town * T ) {
-  /* TODO */
+    R->U = T;
 }
 
 struct town * getVRoad(struct road * R) {
-    return 0;
+    return R->V;
 }
 
 void setVRoad ( struct road * R, struct town * T ) {
-/* TODO */}
+    R->V = T;
+}
 
 void viewRoad ( struct road * R ) {
-  /* TODO */
+    printf("%s <----> %s \n",getTownName(R->U),getTownName(R->V));
+}
+
+struct list * readFileFromRoad ( char * filename ){
+    struct list * L = new();
+    FILE * file = fopen(filename, "r");
+    int nbRoute;
+
+    if (file == NULL)
+    {
+        printf("Le fichier %s n'a pas pu être ouvert\n", filename);
+        exit(1);
+    }
+
+    fscanf(file, " %d", &nbRoute);
+
+    for ( int i = 0; i < nbRoute; i++ ) {
+        char villeU[BUFSIZ], villeV[BUFSIZ];
+        fscanf(file," ( %s , %s )",villeU, villeV);
+
+        int buffLen = strlen(villeU) + 1;
+        char * nameU = ( char * ) calloc(buffLen, sizeof(char));
+        strcpy(nameU, villeU);
+
+        buffLen = strlen(villeV) + 1;
+        char * nameV = ( char * ) calloc(buffLen, sizeof(char));
+        strcpy(nameV, villeV);
+        
+        struct road * R = createRoad(createTown(nameU),createTown(nameV));
+        cons(L,R);
+    }
+    fclose(file);
+
+    return L;
 }
